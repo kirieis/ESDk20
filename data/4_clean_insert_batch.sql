023fb56cf049a5538e294f8501cde9d079978384
kirieis <nguyenansuper123@gmail.com>
2026-01-20 16:13:48 +0700

    update SQL file

--- data/4_clean_insert_batch.sql
+++ data/4_clean_insert_batch.sql
@@ -1,15 +1,11 @@
-INSERT INTO Batch (batch_id, medicine_id, expiry_date, quantity_vien, branch_id)
-SELECT
-    batch_id,
+USE PharmacyDB;
+GO
+
+INSERT INTO Medicine (medicine_id, name, unit)
+SELECT DISTINCT
     medicine_id,
-    TRY_CONVERT(DATE, expiry_date),
-    TRY_CONVERT(INT, quantity_vien),
-    branch_id
+    medicine_name,
+    'VIEN'
 FROM Batch_Staging
-WHERE
-    batch_id IS NOT NULL
-    AND batch_id <> ''
-    AND medicine_id IS NOT NULL
-    AND medicine_id <> ''
-    AND TRY_CONVERT(DATE, expiry_date) IS NOT NULL
-    AND TRY_CONVERT(INT, quantity_vien) > 0;
+WHERE medicine_id IS NOT NULL
+  AND medicine_id <> '';
