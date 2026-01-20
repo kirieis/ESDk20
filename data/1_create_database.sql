4f8157b5f95efcdc948d1c98ff052c201063d993
kirieis <nguyenansuper123@gmail.com>
2026-01-20 15:55:14 +0700

    Update 1_create_database.sql

--- data/1_create_database.sql
+++ data/1_create_database.sql
@@ -1,8 +1,4 @@
-CREATE TABLE Medicine (
-    medicine_id VARCHAR(10) PRIMARY KEY,
-    name NVARCHAR(100),
-    unit VARCHAR(10),
-    vien_per_vi INT,
-    vi_per_hop INT,
-    hop_per_thung INT
-);
+CREATE DATABASE PharmacyDB;
+GO
+USE PharmacyDB;
+GO
