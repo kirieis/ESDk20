c4f50547d6927b292875aabd9abe05b3349d99ac
Q.Thinh <tquocthinhak2005@gmail.com>
2026-01-26 16:26:57 +0700

    Update Medicine.java

--- src/core_app/model/Medicine.java
+++ src/core_app/model/Medicine.java
@@ -2,17 +2,19 @@ package core_app.model;
 
 public class Medicine {
 
-    private int medicineId;
+    private String medicineId;
     private String medicineCode;
     private String medicineName;
     private String activeIngredient;
     private String registrationNumber; // Số đăng ký Bộ Y Tế
     private MedicineGroup group;        // DRUG, SUPPLEMENT, MEDICAL_DEVICE
 
+    public Medicine() {
+    }
 
-    public Medicine(int medicineId, String medicineCode, String medicineName,
-            String activeIngredient, String registrationNumber,
-            MedicineGroup group) {
+    public Medicine(String medicineId, String medicineCode, String medicineName,
+                    String activeIngredient, String registrationNumber,
+                    MedicineGroup group) {
         this.medicineId = medicineId;
         this.medicineCode = medicineCode;
         this.medicineName = medicineName;
@@ -22,51 +24,28 @@ public class Medicine {
     }
 
     // ===== Getter & Setter =====
-    public int getMedicineId() {
-        return medicineId;
-    }
 
-    public void setMedicineId(int medicineId) {
-        this.medicineId = medicineId;
+    public String getMedicineId() {
+        return medicineId;
     }
 
     public String getMedicineCode() {
         return medicineCode;
     }
 
-    public void setMedicineCode(String medicineCode) {
-        this.medicineCode = medicineCode;
-    }
-
     public String getMedicineName() {
         return medicineName;
-    }
-
-    public void setMedicineName(String medicineName) {
-        this.medicineName = medicineName;
     }
 
     public String getActiveIngredient() {
         return activeIngredient;
     }
 
-    public void setActiveIngredient(String activeIngredient) {
-        this.activeIngredient = activeIngredient;
-    }
-
     public String getRegistrationNumber() {
         return registrationNumber;
     }
 
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
     public MedicineGroup getGroup() {
         return group;
-    }
-
-    public void setGroup(MedicineGroup group) {
-        this.group = group;
     }
 }
